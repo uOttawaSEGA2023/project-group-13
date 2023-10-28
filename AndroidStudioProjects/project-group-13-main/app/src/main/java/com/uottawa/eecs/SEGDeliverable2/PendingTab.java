80b5d44894e31c502325bdab7114aaaccdd61872
lydiaroy <lydiaelizabeth004@gmail.com>
2023-10-28 15:11:12 -0400

    Committing files for deliverable 2

---
+++ AndroidStudioProjects/project-group-13-main/app/src/main/java/com/uottawa/eecs/SEGDeliverable2/PendingTab.java
@@ -0,0 +1,53 @@
+package com.uottawa.eecs.SEGDeliverable2;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+//basically the same as the rejected tab just for pending instead
+
+public class PendingTab extends Fragment {
+
+    RecyclerView recyclerView;
+    PendingItemAdapter pendingItemAdapter;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View originView =  inflater.inflate(R.layout.fragment_pending_tab, container, false);
+
+        recyclerView = (RecyclerView) originView.findViewById(R.id.PendingRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        // fills the recycler with the information
+        FirebaseRecyclerOptions<DataClass> options =
+                new FirebaseRecyclerOptions.Builder<DataClass>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Users").child("Pending"), DataClass.class)
+                        .build();
+
+        pendingItemAdapter = new PendingItemAdapter(options);
+        recyclerView.setAdapter(pendingItemAdapter); // adapts the pending item into a card that can be displayed
+
+       return originView;
+    }
+
+
+    // these allow us to update in real time from the database
+    @Override
+    public void onStart() {
+        super.onStart();
+        pendingItemAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        pendingItemAdapter.stopListening();
+    }
+}
---
+++ AndroidStudioProjects/project-group-13-main/app/src/main/java/com/uottawa/eecs/SEGDeliverable2/Welcomepage.java
@@ -0,0 +1,54 @@
+package com.uottawa.eecs.SEGDeliverable2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+// welcome page which is shown after login
+public class Welcomepage extends AppCompatActivity {
+
+    Button continuebutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomepage);
+
+        TextView textView = (TextView) findViewById(R.id.Occupation);
+        Bundle userTypes = getIntent().getExtras();
+
+        if(userTypes != null){
+            String currentOccupation = userTypes.getString("Occupation");
+            textView.setText("Welcome! You are logged in as a " + currentOccupation);
+        }
+
+        continuebutton = findViewById(R.id.continueButton);
+
+        continuebutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                if(userTypes.getString("Occupation").equals("Admin")) {
+                    OpenAdminHomePage();
+                } else {
+                    OpenLogoutPage();
+                }
+            }
+     });
+    }
+
+    public void OpenLogoutPage(){
+        Intent intent = new Intent(this, LogoutPage.class);
+        startActivity(intent);
+    }
+
+    public void OpenAdminHomePage(){
+        Intent intent = new Intent(this, AdminHomePage.class);
+        startActivity(intent);
+    }
+
+
+}
